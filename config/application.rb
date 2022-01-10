65118e78445c2acd246d0280ddcb7fd6b4a1c245
hata-junnosuke <jh19911209@icloud.com>
2022-01-10 11:13:04 +0900

    [Add]generators時に不要なファイルを生成しないような設定

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,14 @@ module InstaClone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators.system_tests = nil
+
+    #generateコマンド時に生成されるファイルを制限する
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
